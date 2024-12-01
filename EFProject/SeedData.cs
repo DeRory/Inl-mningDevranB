1f11eafe51959863123d84205882a2ab355e497a
Devran Bulduk <devranrodibulduk@gmail.com>
2024-12-01 15:57:46 +0100

    AddedMultipleAuhtorBook

--- EFProject/SeedData.cs
+++ EFProject/SeedData.cs
@@ -15,7 +15,7 @@ public static class SeedData
                 new Author { Name = "George R.R. Martin" },
                 new Author { Name = "J.R.R. Tolkien" },
                 new Author { Name = "E. Aladdin" }
-                // ny exempel
+                
             );
         }
 
@@ -26,7 +26,9 @@ public static class SeedData
                 new Book { Title = "Harry Potter and the Sorcerer's Stone", PublishedYear = "1997", Genre = "Fantasy" },
                 new Book { Title = "Game of Thrones", PublishedYear = "1996", Genre = "Fantasy" },
                 new Book { Title = "The Hobbit", PublishedYear = "1937", Genre = "Fantasy" },
-                new Book { Title = "Aladdin and the pupils", PublishedYear = "2024", Genre = "Educational"}
+                new Book { Title = "Aladdin and the pupils", PublishedYear = "2024", Genre = "Educational"},
+                new Book { Title = "The Holy Trinity", PublishedYear = "1999", Genre = "Fantasy"}
+
             );
         }
 
@@ -37,11 +39,12 @@ public static class SeedData
                 new BookAuthor { BookID = 1, AuthorID = 1 },
                 new BookAuthor { BookID = 2, AuthorID = 2 },
                 new BookAuthor { BookID = 3, AuthorID = 3 },
-                new BookAuthor { BookID = 4, AuthorID = 4 }
+                new BookAuthor { BookID = 4, AuthorID = 4 },
+                new BookAuthor { BookID = 5, AuthorID = 2 },
+                new BookAuthor { BookID = 5, AuthorID = 3 },
+                new BookAuthor { BookID = 5, AuthorID = 4 }
             );
         }
-
-        
         context.SaveChanges();
     }
 }
